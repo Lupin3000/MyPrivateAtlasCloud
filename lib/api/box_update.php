b7a53f74c173b8de63c2cf2bc427575c3d833c16
Lupin3000 <lupin@MacBookPro.local>
2018-04-13 16:18:36 +0200

    needs to be fixed upload

--- lib/api/box_update.php
+++ lib/api/box_update.php
@@ -4,6 +4,36 @@ session_start();
 $ini_array = parse_ini_file('../config/application.ini', true);
 $response = array();
 
+function transform_input($value='')
+{
+  $value = trim($value);
+  $value = htmlspecialchars($value);
+  return $value;
+}
+
+function check_file_upload()
+{
+  global $response;
+
+  $json_file = str_replace('/', '_', transform_input($_POST['boxname'])) . '.json';
+
+  $response['status'] = false;
+  $response['message'] = 'Debug/Develop: file upload check' . $json_file;
+}
+
+if ((isset($_SESSION['valid'])) && (isset($_SESSION['user']))) {
+  if (strcmp($_SERVER['REQUEST_METHOD'], 'POST') == 0 && (isset($_POST['boxname'])) && (isset($_POST['boxdescription'])))
+  {
+    check_file_upload();
+  } else {
+    $response['status'] = false;
+    $response['message'] = 'Bad request';
+  }
+} else {
+  $response['status'] = false;
+  $response['message'] = 'Access to content prohibited';
+}
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=UTF-8');
 echo json_encode($response);
