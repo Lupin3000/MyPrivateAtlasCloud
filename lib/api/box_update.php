274f157e187f9d07b11e985a8770647d7791dd44
Steffen Lorenz <steffen.lorenz@haufe.com>
2018-04-13 12:50:13 +0200

    prepare update

--- lib/api/box_update.php
+++ lib/api/box_update.php
@@ -1,3 +1,10 @@
 <?php
 session_start();
+
+$ini_array = parse_ini_file('../config/application.ini', true);
+$response = array();
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=UTF-8');
+echo json_encode($response);
 ?>
